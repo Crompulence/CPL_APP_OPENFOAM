6101c0495cdd84f3bf291f532c9059dc632ba4a8
Adnan Sufian <a.sufian@imperial.ac.uk>
2018-05-03 20:47:26 +0100

    Added hydrostatic case (using dummy script with no particles). Updated Makefile to include 'make examples' which runs py.tests on example cases

---
+++ examples/hydrostatic/run.sh
@@ -0,0 +1,14 @@
+#Set OpenFOAM folder
+OPEN_FOAM_CASE=openfoam/
+
+#Clean CFD
+cd ${OPEN_FOAM_CASE}
+python clean.py -f
+rm -f ../log.openfoam
+blockMesh
+decomposePar
+cd ../
+
+#Run job
+CFD_EXE="$(which CPLSediFOAM)"
+cplexec -c 1 "${CFD_EXE} -case ${OPEN_FOAM_CASE} -parallel > log.openfoam" -m 1 "./MD_no_particle.py"
