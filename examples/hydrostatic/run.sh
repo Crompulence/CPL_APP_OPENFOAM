988f22c61f3217fc60edc1267bfdbc10767b6a31
edwardsmith999 <edward.smith05@imperial.ac.uk>
2018-05-21 10:17:27 +0100

    Added verbose output to run.sh

--- examples/hydrostatic/run.sh
+++ examples/hydrostatic/run.sh
@@ -11,4 +11,4 @@ cd ../
 
 #Run job
 CFD_EXE="$(which CPLSediFOAM)"
-cplexec -c 1 "${CFD_EXE} -case ${OPEN_FOAM_CASE} -parallel > log.openfoam" -m 1 "./MD_no_particle.py" -p
+cplexec -pv -c 1 "${CFD_EXE} -case ${OPEN_FOAM_CASE} -parallel > log.openfoam" -m 1 "./MD_no_particle.py"
