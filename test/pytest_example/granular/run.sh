55b8bd260b403fc7264503b06cf2232b03c2a2b4
edwardsmith999 <edward.smith05@imperial.ac.uk>
2018-09-17 17:55:47 +0100

    Renamed granular to coupled_to_pytest

--- test/pytest_example/granular/run.sh
+++
@@ -1,9 +0,0 @@
-
-cd openfoam
-python clean.py -f
-blockMesh
-decomposePar
-cd ../
-
-mpiexec -n 1 CPLSediFOAM -case ./openfoam -parallel : -n 1 py.test -v  ./python_dummy/test_vs_couette_analytical_aspytest.py
-
